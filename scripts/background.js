eed3d6a02765c1e101cf27f9a0452d5b2ecc4d12
Trevor Reznick <electrics.sheeps@gmail.com>
2025-01-11 10:44:28 +0100

    OPF-84 staging

--- scripts/background.js
+++ scripts/background.js
@@ -1,6 +1,15 @@
 
 console.log("Service worker attivato!")
 
+chrome.runtime.onMessageExternal.addListener((request, sender, sendResponse) => {
+  if (request.type === 'login') {
+      console.log('User logged in:', request.user)
+      // Esegui le azioni necessarie quando l'utente si logga
+      sendResponse({ status: 'success' })
+  }
+  return true// Indica che invierai una risposta asincrona
+})
+
 chrome.cookies.get({url: "https://openfav-dev.vercel.app", name: "sb-access-tocken"}, function(cookie) {
   if (chrome.runtime.lastError) {
     //console.error("Errore nell'accesso ai cookie:", chrome.runtime.lastError)
